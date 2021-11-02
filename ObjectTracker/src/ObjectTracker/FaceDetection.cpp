fb954ec177ec32bae05dcb986927263b01bd19df
Ylenia Battistini <ylenia.battistini@studio.unibo.it>
2021-11-02 14:12:14 +0100

    face detection bug??!

--- ObjectTracker/src/ObjectTracker/FaceDetection.cpp
+++ ObjectTracker/src/ObjectTracker/FaceDetection.cpp
@@ -7,7 +7,7 @@
 
 FaceDetection::FaceDetection() : scale(1) {
 
-	if (cascade.load("./haarcascade_frontalface_alt.xml") == true) {
+	if (cascade.load("./haarcascade_frontalface_default.xml") == true) {
 		cout << "Face Detection Started.." << endl;
 	}
 	else
@@ -16,14 +16,12 @@ FaceDetection::FaceDetection() : scale(1) {
 
 void FaceDetection::Detection(Mat& image)
 {
-	Mat frame;
 	cout << "Face Detection Started.." << endl;
-
+	
 	if (image.empty())
 		throw std::runtime_error("Empty image");
 
-	Mat imageToDetect = image.clone();
-	DetectFace(imageToDetect);
+	DetectFace(image);
 
 }
 
@@ -32,19 +30,19 @@ void FaceDetection::DetectFace(Mat& img) {
 
 	vector<cv::Rect> faces;
 	Mat gray, smallImg;
+	Mat equalizeImg;
 
 	cvtColor(img, gray, COLOR_BGR2GRAY);
 	double fx = 1 / scale;
-	Size_<int> size =  Size_<int>(0,0);
 
-	resize(gray, smallImg, size, fx, fx, INTER_LINEAR_EXACT);
-	equalizeHist(smallImg, smallImg);
+	resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT);
+	equalizeHist(smallImg, equalizeImg);
 
-	cascade.detectMultiScale(smallImg, faces , 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+	cascade.detectMultiScale(equalizeImg, faces);
 
 	DrawElement::DrawCircle(faces, scale, img);
 	int i = 0;
 	i++;
-	imwrite("C:\\Users\\ylenia\\Desktop\\output.jpg", img);
+	//imwrite("C:\\Users\\ylenia\\Desktop\\output.jpg", img);
 
 }
